65d81700e912115d723065f7b977c260688fecb3
sushmoyr <sushmoyr@gmail.com>
2022-02-26 20:07:13 +0600

    resource managers

--- lib/presentation/resources/styles_manager.dart
+++ lib/presentation/resources/styles_manager.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'font_manager.dart';
+import 'package:mvvm_clean_architecture/presentation/resources/font_manager.dart';
 
 TextStyle _getTextStyle(
     double fontSize, String fontFamily, FontWeight fontWeight, Color color) {
